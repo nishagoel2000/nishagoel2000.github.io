5b239891baa1386b83d7b5d07ef80c99e2d29194
nishagoel2000 <65680543+nishagoel2000@users.noreply.github.com>
2024-12-17 11:51:04 -0500

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -8,12 +8,15 @@
 		for (i = 0; i < slides.length; i++) {
 			slides[i].style.display = "none";  
 		}
-		slideIndex++;
-		if (slideIndex > slides.length) {slideIndex = 1}    
+		
+		slideIndex = (slideIndex + 1) % slides.length;
+		if (slideIndex > slides.length) {
+			slideIndex = 1
+		}    
 		for (i = 0; i < dots.length; i++) {
 			dots[i].className = dots[i].className.replace(" active", "");
 		}
 		slides[slideIndex-1].style.display = "block";  
 		dots[slideIndex-1].className += " active";
 		setTimeout(showSlides, 3000); // Change image every 3 seconds
-		}
+	}
