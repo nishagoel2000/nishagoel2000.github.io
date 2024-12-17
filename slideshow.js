2ff6808586f6a9747515de4f8f2050149ae36de7
nishagoel2000 <65680543+nishagoel2000@users.noreply.github.com>
2024-12-17 11:56:07 -0500

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -7,12 +7,12 @@
 		var dots = document.getElementsByClassName("dot");
 		for (i = 0; i < slides.length; i++) {
 			slides[i].style.display = "none";  
+			slideIndex++;
+			if (slideIndex > slides.length) {
+				slideIndex = 1
+			} 
 		}
-		
-		slideIndex = (slideIndex + 1) % slides.length;
-		if (slideIndex > slides.length) {
-			slideIndex = 1
-		}    
+   
 		for (i = 0; i < dots.length; i++) {
 			dots[i].className = dots[i].className.replace(" active", "");
 		}
